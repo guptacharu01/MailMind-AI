485c0056c073bfcd9259d4481be25fac35c07a10
Charu <gupta.charu2004@gmail.com>
2025-10-31 21:59:30 +0530

    Moved backend files to correct folder

--- src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,5 +1,6 @@
 package com.email.writer.app;
 
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
