0ba158bb8896d529879179449780afac48c82c98
Charu <gupta.charu2004@gmail.com>
2025-10-31 20:25:42 +0530

    Initial commit - backend

---
+++ src/main/java/com/email/writer/EmailWriteSbApplication.java
@@ -0,0 +1,13 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriteSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriteSbApplication.class, args);
+	}
+
+}
